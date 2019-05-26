21f9aa2593d13062b405505a3c588064979a525b
Fragarach95 <37145259+Fragarach95@users.noreply.github.com>
2019-05-26 18:51:12 +0200

    Circle + Wall model implementering begyndt

--- YourTime/YourTime/Models/Circle.cs
+++ YourTime/YourTime/Models/Circle.cs
@@ -2,10 +2,26 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
 
 namespace YourTime.Models
 {
     public class Circle
     {
+
+        public Circle()
+        {
+            UsersId = new List<string>();
+        }
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonElement("CircleName")]
+        public string Circlename { get; set; }
+
+        [BsonElement("CircleUsers")]
+        public List<string> UsersId { get; set; }
     }
 }
